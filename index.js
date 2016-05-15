41eabee89c1dcfc0e133a1e4061ff7125dccd3e2
Rob Loach <robloach@gmail.com>
2016-05-15 16:17:20 -0500

    0.0.3

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var physfs = require('./physfs.js');
 // Clean up and structure.
 var functions = physfs.functions;
 delete physfs.functions;
-var constants = physfs.CONSTANTS;
+var consts = physfs.CONSTANTS;
 delete physfs.CONSTANTS;
 
 // Use extend-shallow to merge the object.
-var out = extend(physfs, constants, functions);
+var out = extend(physfs, consts, functions);
 
 // Remove the PHYSFS_ prefix from each constant/function.
 for (var name in out) {
