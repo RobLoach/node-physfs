bb28e1635586e95f8d7c64d820b174b5a374e825
Rob Loach <robloach@gmail.com>
2016-05-15 16:13:27 -0500

    Clean up function and constants

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+//Dependencies
 var extend = require('extend-shallow');
 var physfs = require('./physfs.js');
 
 // Clean up and structure.
 var functions = physfs.functions;
 delete physfs.functions;
+var constants = physfs.CONSTANTS;
+delete physfs.CONSTANTS;
 
 // Use extend-shallow to merge the object.
-module.exports = extend(physfs, functions);
+var out = extend(physfs, constants, functions);
+
+// Remove the PHYSFS_ prefix from each constant/function.
+for (var name in out) {
+  out[name.replace('PHYSFS_', '')] = out[name];
+  delete out[name];
+}
+
+module.exports = out;
